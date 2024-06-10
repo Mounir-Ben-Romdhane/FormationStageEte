cff536d1f5cfc02fcde8926124d3d66611ce856b
Mounir Ben Romdhane <mounir.benromdhane@esprit.tn>
2024-06-10 22:09:29 +0100

    fix version

--- app/src/main/java/com/example/formationgroupe8/MainActivity.java
+++ app/src/main/java/com/example/formationgroupe8/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.formationgroupe8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Declaration des variables
+    private ImageView image;
+    private Button btnShow, btnHide;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Affectation des views
+        image = findViewById(R.id.imageToHideAndShow);
+        btnShow = findViewById(R.id.btnShow);
+        btnHide = findViewById(R.id.btnHide);
+
+        //First case
+        btnShow.setOnClickListener(v -> {
+            //Action after click to button
+            image.setVisibility(View.VISIBLE);
+        });
+
+        //Second case
+        btnHide.setOnClickListener(v -> {
+            image.setVisibility(View.INVISIBLE);
+        });
+
     }
 }
